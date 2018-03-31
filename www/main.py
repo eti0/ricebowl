314cb0a36a4e77d391140de24c20168d707ad997
Naïm Favier <fnaim42@gmail.com>
2018-04-01 05:39:00 +1200

    basic skeleton + design

--- www/main.py
+++ www/main.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, render_template
+from flaskext.markdown import Markdown
 
-@app = Flask(__name__)
+app = Flask(__name__)
+Markdown(app)
 
 @app.route('/')
-def rice():
-    return 'rice bowl!'
+def index():
+    return render_template('index.html')
 
+@app.route('/submit')
+def submit():
+    return render_template('submit.html', title='submit')
+
+@app.route('/vote')
+def vote():
+    return render_template('vote.html', title='vote')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='about')
